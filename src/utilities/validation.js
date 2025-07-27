fb87c8815d1a0e6a15592026d57ebb49f3c9ba2e
uth.gbolahan@gmail.com <uth.gbolahan@gmail.com>
2025-07-27 11:54:26 +0200

    =modify isnumber function in validation.js

--- src/utilities/validation.js
+++ src/utilities/validation.js
@@ -3,3 +3,7 @@ export function isText(value){
   if(value.length <= 1) throw new Error("author name not valid")
  return _value
 }
+export  function IsNumber(value){
+    const _value = typeof value === typeof 0? true:false
+    return _value
+} 
